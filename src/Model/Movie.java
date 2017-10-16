a103cd7a20e05ee4ac4e5832201a02efde2c43e1
unknown <HADI0006@e.ntu.edu.sg>
2017-10-16 21:36:52 +0800

    added model

---
+++ src/Model/Movie.java
@@ -0,0 +1,14 @@
+package Model;
+
+public class Movie {
+	private String movieTitle;
+	private String showingStatus;
+	private String synopsis;
+	private String director;
+	private String[] cast;
+	private double overallRating;
+	
+	public Movie(){
+		
+	}
+}
