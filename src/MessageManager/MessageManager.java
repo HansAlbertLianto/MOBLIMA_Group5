952f50b6633aa5489b86e3c926863aa7c1b60147
hadisiswanto62 <HADI0006@e.ntu.edu.sg>
2017-10-25 22:01:55 +0800

    add messagemanager

--- src/MessageManager/MessageManager.java
+++ src/MessageManager/MessageManager.java
@@ -1,18 +1,21 @@
 package MessageManager;
+
 import java.util.Scanner;
 
 public class MessageManager {
-	
+
 	private static MessageManager singleton = null;
 	private static Scanner sc = new Scanner(System.in);
-	
-	private MessageManager() {}
-	
+
+	private MessageManager() {
+	}
+
 	public static MessageManager getInstance() {
-		if(singleton == null) singleton = new MessageManager();
+		if (singleton == null)
+			singleton = new MessageManager();
 		return singleton;
 	}
-	
+
 	public void printMessage(String msg) {
 		System.out.println(msg);
 	}
@@ -26,6 +29,20 @@ public class MessageManager {
 			isValid = input>=from && input<=to;
 			if (!isValid){
 				System.out.printf("Invalid choice! Choice must be between %d and %d\n",from,to);
+			}
+		}
+		return input;
+	}
+
+	public String inputString(int minLength, int maxLength, String caption){
+		String input = "";
+		boolean isValid = false;
+		while (!isValid){
+			System.out.printf(caption + " = ");
+			input = sc.next();
+			isValid = input.length() >= minLength && input.length() <= maxLength;
+			if (!isValid){
+				System.out.printf("Invalid input! Input length must be between %d and %d\n", minLength, maxLength);
 			}
 		}
 		return input;
