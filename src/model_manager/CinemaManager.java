0a9f18ef2e6b59d46061245691aeeb2ed22542c4
user <andrekristanto17@gmail.com>
2017-10-30 16:39:05 +0800

    change action service

--- src/model_manager/CinemaManager.java
+++ src/model_manager/CinemaManager.java
@@ -1,5 +1,12 @@
 package model_manager;
 
+import model.Cinema;
+import model.Cineplex;
+import model.Movie;
+
 public class CinemaManager {
 
+    public Cinema getAllCinemas(Cineplex cineplex){
+        return null;
+    }
 }
--- src/service/ActionService.java
+++ src/service/ActionService.java
@@ -1,8 +1,18 @@
 package service;
 
+import model.Cinema;
+import model.CinemaMovie;
+import model.Cineplex;
+
+import java.util.ArrayList;
+
 public interface ActionService {
 
     public void doLogin(String username, String password);
+
+    // ----------------------------------
+    // Movie Action
+    // ----------------------------------
 
     public void doSearchMovie();
 
@@ -10,14 +20,35 @@ public interface ActionService {
 
     public void doPrintMovieDetails();
 
-    public void doCheckSeat();
+    //  ----------------------------------
+    //  Cineplex Action
+    //  ----------------------------------
+    public ArrayList<Cineplex> doGetAllCineplex();
 
-    public void doBook();
+    // ----------------------------------
+    // Cinema Action
+    // ----------------------------------
+    public ArrayList<Cinema> doGetAllCinema(Cineplex cineplex);
 
-    public void doPurchase();
+    //  ----------------------------------
+    //  Cinema Movie Action
+    //  ----------------------------------
+    public ArrayList<CinemaMovie> doGetAllCinemaMovie(Cinema cinema);
 
+    public void doCheckSeat(CinemaMovie cinemaMovie);
+
+    public void doBook(CinemaMovie cinemaMovie);
+
+    public void doPurchase(CinemaMovie cinemaMovie);
+
+    //  ----------------------------------
+    //  Movie-goers Action
+    //  ----------------------------------
     public void doFindHistory();
 
+    //  ----------------------------------
+    //  Admin Action
+    //  ----------------------------------
     public void doChangeMovieDetails();
 
     public void doAddMovieToCinema();
